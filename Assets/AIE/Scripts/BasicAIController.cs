413912fcd741d000f8537981c8381a32b1452d8b
Terry Nguyen <terry@terrehbyte.com>
2023-02-10 09:44:13 -0800

    Clean-up and document Agent code

--- Assets/AIE/Scripts/BasicAIController.cs
+++ Assets/AIE/Scripts/BasicAIController.cs
@@ -6,11 +6,32 @@ public class BasicAIController : MonoBehaviour
 {
     public HumanoidMotor motor;
 
+    /// <summary>
+    /// Will remain crouched for at least this long before standing back up
+    /// </summary>
+    public float crouchMinTime = 0.1f;
+    /// <summary>
+    /// Timer used to determine when standing back up is possible
+    /// </summary>
+    private float crouchTimer;
+
+    [Header("Waypoints")]
+    /// <summary>
+    /// A series of waypoints that the character will walk through. Will loop back to
+    /// beginning after reaching the last one.
+    /// </summary>
     public Transform[] waypoints;
+    /// <summary>
+    /// An index denoting which waypoint we are currently pathing to.
+    /// </summary>
     private int currentWaypoint;
 
-    public float threshold = 0.3f;
+    /// <summary>
+    /// 
+    /// </summary>
+    public float waypointThreshold = 0.3f;
 
+    [Header("Sweep")]
     public LayerMask worldMask;
 
     private Collider[] overlapResults = new Collider[32];
@@ -18,13 +39,15 @@ public class BasicAIController : MonoBehaviour
 
     private void Update()
     {
+        bool crouchedThisFrame = false;
+
         // waypoint logic
         Vector3 curPos = motor.transform.position;
         Vector3 dstPos = waypoints[currentWaypoint].position;
 
         Vector3 offset = dstPos - curPos;
 
-        if(offset.sqrMagnitude < threshold * threshold)
+        if(offset.sqrMagnitude < waypointThreshold * waypointThreshold)
         {
             ++currentWaypoint;
             currentWaypoint %= waypoints.Length;
@@ -35,19 +58,18 @@ public class BasicAIController : MonoBehaviour
 
         offset = Vector3.ClampMagnitude(offset, 1.0f);
 
+        // apply desired input
         motor.MoveWish = offset;
 
         //
-        // check if crouch needed
+        // check if movement will be blocked
         //
-
         HumanoidMotor.GetCapsulePoints(motor.col.height, motor.col.radius,
             out var top, out _, out var bottom);
         top = motor.transform.TransformPoint(top);
         bottom = motor.transform.TransformPoint(bottom);
 
         bool isBlocked = Physics.CapsuleCast(top, bottom, motor.col.radius * 0.95f, offset.normalized, motor.MoveSpeed * Time.deltaTime);
-        bool crouchedThisFrame = false;
         // don't evaluate crouch-bypass if already crouched and blocked
         if(isBlocked && !motor.CrouchWish)
         {
@@ -64,6 +86,7 @@ public class BasicAIController : MonoBehaviour
                 motor.MoveSpeed * Time.deltaTime);
             motor.CrouchWish = shouldCrouch;
             crouchedThisFrame = shouldCrouch;
+            crouchTimer = crouchMinTime;
         }
 
         //
@@ -71,16 +94,20 @@ public class BasicAIController : MonoBehaviour
         //
         if(!crouchedThisFrame && motor.CrouchWish)
         {
-            HumanoidMotor.GetCapsulePoints(motor.StandHeight, motor.col.radius,
-            out top, out _, out bottom);
-            top = motor.transform.TransformPoint(top);
-            bottom = motor.transform.TransformPoint(bottom);
-
-            overlapCount = Physics.OverlapCapsuleNonAlloc(top, bottom, motor.col.radius * 0.95f, overlapResults, worldMask);
-            // nothing in the way?
-            if (overlapCount == 0)
+            crouchTimer -= Time.deltaTime;
+            if (crouchTimer < 0)
             {
-                motor.CrouchWish = false;
+                HumanoidMotor.GetCapsulePoints(motor.StandHeight, motor.col.radius,
+                out top, out _, out bottom);
+                top = motor.transform.TransformPoint(top);
+                bottom = motor.transform.TransformPoint(bottom);
+
+                overlapCount = Physics.OverlapCapsuleNonAlloc(top, bottom, motor.col.radius * 0.95f, overlapResults, worldMask);
+                // nothing in the way?
+                if (overlapCount == 0)
+                {
+                    motor.CrouchWish = false;
+                }
             }
         }
     }
--- Assets/AIE/Scripts/HumanoidMotor.cs
+++ Assets/AIE/Scripts/HumanoidMotor.cs
@@ -5,23 +5,36 @@ using UnityEngine;
 [SelectionBase]
 public class HumanoidMotor : MonoBehaviour
 {
-    public Vector3 MoveWish;
-    public bool CrouchWish;
-
-    public float MoveSpeed = 8.0f;
-
-    public float StandHeight = 2.0f;
-    public float CrouchHeight = 1.25f;
-
     [field: SerializeField]
     public Rigidbody rbody { get; private set; }
     [field: SerializeField]
     public CapsuleCollider col { get; private set; }
 
+    [Header("Input")]
+    public Vector3 MoveWish;
+    public bool CrouchWish;
+
+    [Header("Motor Settings")]
+    public float MoveSpeed = 8.0f;
+    public float CrouchSpeed = 6.0f;
+    [Space]
+    public float StandHeight = 2.0f;
+    public float CrouchHeight = 1.25f;
+
+    public struct CapsuleDimensions
+    {
+        public Vector3 top;
+        public Vector3 center;
+        public Vector3 bottom;
+    }
+
     // TODO: add a way to retrieve dimensions (top, center, bot) for the capsule
     // at specific heights
-    public static void GetCapsulePoints(float height, float radius, out Vector3 top, out Vector3 center, out Vector3 bottom)
+    public static void GetCapsulePoints(float height, float radius,
+        out Vector3 top, out Vector3 center, out Vector3 bottom)
     {
+        // assumes that the pivot is bottom-center
+
         center = new Vector3(0, height / 2, 0);
 
         Vector3 offset = new Vector3(0, height / 2 - radius, 0);
@@ -30,18 +43,38 @@ public class HumanoidMotor : MonoBehaviour
         bottom = center - offset;
     }
 
+    public static CapsuleDimensions GetCapsulePoints(float height, float radius)
+    {
+        // assumes that the pivot is bottom-center
+
+        CapsuleDimensions dims = new CapsuleDimensions();
+        dims.center = new Vector3(0, height / 2, 0);
+
+        Vector3 offset = new Vector3(0, height / 2 - radius, 0);
+
+        dims.top = dims.center + offset;
+        dims.bottom = dims.center - offset;
+
+        return dims;
+    }
+
     private void ResizeHeight(float newHeight)
     {
+        // assumes that the pivot is bottom-center
+
         col.height = newHeight;
         col.center = new Vector3(0, col.height / 2, 0);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        MoveWish *= MoveSpeed;
+        // calculate movement
+        Vector3 delta = MoveWish * (CrouchWish ? CrouchSpeed : MoveSpeed);
 
+        // apply standing/crouching
         ResizeHeight(CrouchWish ? CrouchHeight : StandHeight);
 
-        rbody.MovePosition(rbody.position + MoveWish * Time.deltaTime);
+        // apply movement
+        rbody.MovePosition(rbody.position + delta * Time.deltaTime);
     }
 }
